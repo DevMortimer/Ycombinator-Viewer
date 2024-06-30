f1da070fe375f49747d46225be28c974085e92db
Ryan Joshua <ryanjoshuagapac@gmail.com>
2024-06-30 21:15:42 +0800

    icons, splash

--- android/app/src/main/java/com/cygntek/hviewer/MainActivity.java
+++ android/app/src/main/java/com/cygntek/hviewer/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.cygntek.hviewer;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic-app-base',
+  appId: 'com.cygntek.hviewer',
+  appName: 'Hackernews Viewer',
   webDir: 'www'
 };
 
